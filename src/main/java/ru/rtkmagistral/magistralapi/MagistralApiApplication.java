3d3505d1433acb97e467bd8d15dd8038631a0561
vodobryshkin <dobryshkinvladimir@yandex.ru>
2026-01-26 17:34:59 +0300

    Добавил Java-скелет проекта

---
+++ src/main/java/ru/rtkmagistral/magistralapi/MagistralApiApplication.java
@@ -0,0 +1,12 @@
+package ru.rtkmagistral.magistralapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagistralApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MagistralApiApplication.class, args);
+    }
+
+}
