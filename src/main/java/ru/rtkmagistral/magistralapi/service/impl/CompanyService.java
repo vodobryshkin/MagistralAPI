2398009f81a7842e9914e90c6a2e7c6b2dea0928
vodobryshkin <dobryshkinvladimir@yandex.ru>
2026-02-01 02:25:57 +0300

    Добавил возможность регистрации для юридических лиц

---
+++ src/main/java/ru/rtkmagistral/magistralapi/service/impl/CompanyService.java
@@ -0,0 +1,84 @@
+package ru.rtkmagistral.magistralapi.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.rtkmagistral.magistralapi.client.dadata.spec.IDadataClient;
+import ru.rtkmagistral.magistralapi.domain.jpa.Company;
+import ru.rtkmagistral.magistralapi.domain.jpa.User;
+import ru.rtkmagistral.magistralapi.dto.company.CreateCompanyRequest;
+import ru.rtkmagistral.magistralapi.exception.CompanyCreditsException;
+import ru.rtkmagistral.magistralapi.mapper.ICompanyMapper;
+import ru.rtkmagistral.magistralapi.repository.CompanyRepository;
+import ru.rtkmagistral.magistralapi.service.spec.ICompanyService;
+
+/**
+ * Сервис для процессов бизнес-логики, связанных с доменной сущностью "Компания".
+ */
+@Service
+@RequiredArgsConstructor
+public class CompanyService implements ICompanyService {
+    private final IDadataClient dadataClient;
+
+    private final ICompanyMapper companyMapper;
+
+    private final CompanyRepository companyRepository;
+
+    /**
+     * Метод для проверки корректности переданных с фронтенда данных путём получения информации из источников Dadata API.
+     *
+     * @param req запрос на создание компании.
+     * @return результат проверки на существование компании.
+     */
+    @Override
+    public boolean verifyData(CreateCompanyRequest req) {
+        String innReq = req.getInn().trim();
+        String kppReq = req.getKpp().trim();
+        String okvedReq = req.getOkved().trim().replace(" ", "");
+        String titleReq = req.getTitle();
+
+        CreateCompanyRequest companyRequestFormClient = dadataClient.findPartyByInn(innReq);
+
+        if (companyRequestFormClient == null) {
+            throw new CompanyCreditsException("COMPANY_WITH_THIS_INN_NOT_EXISTS_IN_DADATA");
+        }
+
+        if (!titleReq.equals(companyRequestFormClient.getTitle())) {
+            throw new CompanyCreditsException("TITLE_NOT_MATCHES_WITH_DADATA");
+        }
+
+        if (!innReq.equals(companyRequestFormClient.getInn())) {
+            throw new CompanyCreditsException("INN_NOT_MATCHES_WITH_DADATA");
+        }
+
+        if (!kppReq.equals(companyRequestFormClient.getKpp())) {
+            throw new CompanyCreditsException("KPP_NOT_MATCHES_WITH_DADATA");
+        }
+
+        if (!okvedReq.equals(companyRequestFormClient.getOkved())) {
+            throw new CompanyCreditsException("OKVED_NOT_MATCHES_WITH_DADATA");
+        }
+
+        return true;
+    }
+
+    /**
+     * Метод для создания компании в системе.
+     *
+     * @param createCompanyRequest DTO с данными для создания компании.
+     * @param user                 пользователь, который привязан к компании.
+     */
+    @Override
+    public void createCompany(CreateCompanyRequest createCompanyRequest, User user) {
+        if (verifyData(createCompanyRequest)) {
+            if (companyRepository.existsCompanyByInn(createCompanyRequest.getInn())) {
+                throw new CompanyCreditsException("COMPANY_ALREADY_EXISTS_IN_DATABASE");
+            }
+
+            Company company = companyMapper.toEntity(createCompanyRequest);
+            company.setUser(user);
+            companyRepository.save(company);
+        } else {
+            throw new CompanyCreditsException("PROBLEMS_WHILE_ADDING_A_COMPANY");
+        }
+    }
+}
--- src/main/java/ru/rtkmagistral/magistralapi/service/spec/IUserService.java
+++ src/main/java/ru/rtkmagistral/magistralapi/service/spec/IUserService.java
@@ -1,5 +1,6 @@
 package ru.rtkmagistral.magistralapi.service.spec;
 
+import ru.rtkmagistral.magistralapi.dto.company.CreateCompanyRequest;
 import ru.rtkmagistral.magistralapi.dto.user.CreateUserRequest;
 import ru.rtkmagistral.magistralapi.dto.user.UserResponse;
 
@@ -8,10 +9,19 @@ import ru.rtkmagistral.magistralapi.dto.user.UserResponse;
  */
 public interface IUserService {
     /**
-     * Метод для создания пользователя в системе (вызывается при регистрации пользователя)
+     * Метод для создания пользователя (физическое лицо) в системе (вызывается при регистрации пользователя)
      *
      * @param createUserRequest данные, необходимые для создания пользователя.
      * @return ответ на создание пользователя.
      */
     UserResponse createUser(CreateUserRequest createUserRequest);
+
+    /**
+     * Метод для создания пользователя (юридическое лицо) в системе (вызывается при регистрации пользователя)
+     *
+     * @param createUserRequest данные, необходимые для создания пользователя.
+     * @param createCompanyRequest данные, необходимые для создания компании.
+     * @return ответ на создание пользователя.
+     */
+    UserResponse createLegalUser(CreateUserRequest createUserRequest, CreateCompanyRequest createCompanyRequest);
 }
